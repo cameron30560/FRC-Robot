eda831725a4f3f6e724e8980931e037e37e57aa3
cameron30560 <61357309+cameron30560@users.noreply.github.com>
2020-02-22 08:24:17 -0800

    Add files via upload

---
+++ Drivesubsystem.java
@@ -0,0 +1,45 @@
+
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivesubsystem extends SubsystemBase {
+  public static VictorSPX lF = new VictorSPX(0);
+  public static VictorSPX lB = new VictorSPX(1);
+  public static VictorSPX rF = new VictorSPX(2);
+  public static VictorSPX rB = new VictorSPX(3);
+  // public static SpeedControllerGroup left = new SpeedControllerGroup(lF,lB );
+  // public static SpeedControllerGroup right = new SpeedControllerGroup(rB,rF);
+
+  /**
+   * Creates a new Drivesubsystem.
+   */
+  public Drivesubsystem() {
+
+  }
+
+  public static void TankDrive( double leftspeed, double rightspeed){
+  lB.set(ControlMode.PercentOutput, leftspeed);
+  lF.set(ControlMode.PercentOutput, leftspeed);
+  rB.set(ControlMode.PercentOutput, rightspeed);
+  rF.set(ControlMode.PercentOutput, rightspeed);
+}
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+public void setDefualtcomand(RunCommand runCommand) {
+}
+}
